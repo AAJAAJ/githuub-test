4de1bd56a204839e1366f178f71ea9fdd9c61dca
AAJAAJ <1246919399@qq.com>
2019-09-28 20:12:44 +0800

    第一次提交测试 提交工程

---
+++ src/test/java/cn/yunhe/test/IndexTest.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package cn.yunhe.test;
+
+import org.junit.Test;
+
+/**
+ * @author 性感渣渣傲在线打bug
+ * 2019年9月28日 下午3:48:06
+ */
+public class IndexTest {
+
+	/**
+	 * 
+	 */
+	@Test
+	private void test() {
+		// TODO Auto-generated method stub
+		System.out.println(1232546464);
+	}
+}
